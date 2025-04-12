11367ec24f114a2101218a01f861d178d2641bc7
sophia vincent <sophire33@gmail.com>
2025-04-12 01:25:32 -0700

    implemented swiper slide feature

--- app/components/Home/WhyChoose/WhyChoose.tsx
+++ app/components/Home/WhyChoose/WhyChoose.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import SectionHeading from '../../Helper/SectionHeading';
 import WhyChooseCard from './WhyChooseCard';
-import Image from 'next/image';
 
 const WhyChoose = () => {
   return (
@@ -9,13 +8,13 @@ const WhyChoose = () => {
         <SectionHeading heading='Why Choose Us' />
         <div className='grid w-[80%] mx-auto grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16 items-center mt-20'>
             <div>
-                <WhyChooseCard image="/public/file.svg" title="Best Price Guarantee"/>
+                <WhyChooseCard image="/images/price2.jpg" title="Best Price Guarantee"/>
             </div>
             <div>
-                <WhyChooseCard image="/public/file.svg" title="Easy & Quick Booking"/>
+                <WhyChooseCard image="/images/booking.jpg" title="Easy & Quick Booking"/>
             </div>
             <div>
-                <WhyChooseCard image="/public/file.svg" title="Customer Care 24/7"/>
+                <WhyChooseCard image="/images/customer1.jpg" title="Customer Care 24/7"/>
             </div>
         </div>
     </div>
